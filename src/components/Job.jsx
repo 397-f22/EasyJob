4407155dfd250ffe91d01d4440253b2c220c0500
Si Woo <siwoo2000@gmail.com>
2022-12-06 17:39:14 -0600

    added their loss filter check

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,143 +1,146 @@
 import "./Jobs.css";
 
 import {
-  BsChevronDown,
-  BsChevronUp,
-  BsFillCalendarCheckFill,
-  BsLink45Deg,
-  BsAlarmFill,
-  BsSpeedometer,
-  BsTrash,
+    BsAlarmFill,
+    BsChevronDown,
+    BsChevronUp,
+    BsFillCalendarCheckFill,
+    BsLink45Deg,
+    BsSpeedometer,
+    BsTrash,
 } from "react-icons/bs";
 import { Button, Card, Dropdown, DropdownButton } from "react-bootstrap";
+import { removeJob, updateDeadline, updateStatus } from "../utilities/firebase";
 
 import Collapse from "react-bootstrap/Collapse";
-import { Status } from "../utilities/constants";
-import { updateDeadline, updateStatus, removeJob } from "../utilities/firebase";
-import { useState } from "react";
 import { DeadlineUpdater } from "./DeadlineUpdater";
+import { Status } from "../utilities/constants";
+import { useState } from "react";
 
 export const Job = ({
-  id,
-  company,
-  jobTitle,
-  appliedOn,
-  status,
-  deadline,
-  user,
-  tab,
+    id,
+    company,
+    jobTitle,
+    appliedOn,
+    status,
+    deadline,
+    user,
+    tab,
 }) => {
-  const [openJob, setOpenJob] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+    const [openJob, setOpenJob] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
-  const appliedDate = new Date(appliedOn).toLocaleString("en-US", {
-    timeZone: "UTC",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
+    const appliedDate = new Date(appliedOn).toLocaleString("en-US", {
+        timeZone: "UTC",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    });
 
-  const deadlineDate = new Date(deadline).toLocaleString("en-US", {
-    timeZone: "UTC",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
+    const deadlineDate = new Date(deadline).toLocaleString("en-US", {
+        timeZone: "UTC",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    });
 
-  var deadlineText;
-  switch (status) {
-    case "Online Assessment":
-      deadlineText = "Due by ";
-      break;
-    case "Interview":
-      deadlineText = "Scheduled for ";
-      break;
-    case "Offered":
-      deadlineText = "Accept by ";
-      break;
-  }
+    var deadlineText;
+    switch (status) {
+        case "Online Assessment":
+            deadlineText = "Due by ";
+            break;
+        case "Interview":
+            deadlineText = "Scheduled for ";
+            break;
+        case "Offered":
+            deadlineText = "Accept by ";
+            break;
+    }
 
-  return (
-    <>
-      <Card className="m-2">
-        <Card.Header onClick={() => setOpenJob(!openJob)}>
-          <div className="job-title">
-            <p></p>
-            <div className="job-title-middle">
-              <p className="company-text">{company}</p> &nbsp; | &nbsp;{" "}
-              <p className="role-text">{jobTitle}</p>
-            </div>
-            {openJob ? <BsChevronUp /> : <BsChevronDown />}
-          </div>
-        </Card.Header>
+    return (
+        <div data-testid={status + " Job"}>
+            <Card className="m-2">
+                <Card.Header onClick={() => setOpenJob(!openJob)}>
+                    <div className="job-title">
+                        <p></p>
+                        <div className="job-title-middle">
+                            <p className="company-text">{company}</p> &nbsp; |
+                            &nbsp; <p className="role-text">{jobTitle}</p>
+                        </div>
+                        {openJob ? <BsChevronUp /> : <BsChevronDown />}
+                    </div>
+                </Card.Header>
 
-        <DeadlineUpdater
-          show={showModal}
-          setShow={setShowModal}
-          userid={user.uid}
-          jobid={id}
-        />
+                <DeadlineUpdater
+                    show={showModal}
+                    setShow={setShowModal}
+                    userid={user.uid}
+                    jobid={id}
+                />
 
-        <Collapse in={openJob}>
-          <Card.Body>
-            <Card.Text>
-              {" "}
-              <BsFillCalendarCheckFill className="icon" /> Applied on{" "}
-              {appliedDate}
-            </Card.Text>
-            {/* <Card.Text> <BsLink45Deg className="icon" /> {url} </Card.Text> */}
-            {tab === "All" ? (
-              <Card.Text>
-                {" "}
-                <BsSpeedometer className="icon" />
-                {status}
-              </Card.Text>
-            ) : (
-              ""
-            )}
-            {deadline && (
-              <Card.Text>
-                {" "}
-                <BsAlarmFill className="icon" />
-                {deadlineText}
-                {deadlineDate}
-              </Card.Text>
-            )}
-            <DropdownButton variant="secondary" title="Change Status">
-              {Object.values(Status).map((status) => (
-                <Dropdown.Item
-                  key={status}
-                  onClick={() => {
-                    if (
-                      status == Status.OA ||
-                      status == Status.Interview ||
-                      status == Status.Offered
-                    ) {
-                      setShowModal(true);
-                    } else {
-                      updateDeadline(user.uid, id, "");
-                    }
-                    updateStatus(user.uid, id, status);
-                  }}
-                >
-                  {status}
-                </Dropdown.Item>
-              ))}
-            </DropdownButton>
-            <Card.Text>
-              {" "}
-              <Button
-                variant="outline-secondary"
-                id="trashIcon"
-                data-testid={`delete-${tab}-${id}`}
-                onClick={() => removeJob(user.uid, id)}
-              >
-                <BsTrash className="icon" />
-              </Button>
-            </Card.Text>
-          </Card.Body>
-        </Collapse>
-      </Card>
-    </>
-  );
+                <Collapse in={openJob}>
+                    <Card.Body>
+                        <Card.Text>
+                            {" "}
+                            <BsFillCalendarCheckFill className="icon" /> Applied
+                            on {appliedDate}
+                        </Card.Text>
+                        {/* <Card.Text> <BsLink45Deg className="icon" /> {url} </Card.Text> */}
+                        {tab === "All" ? (
+                            <Card.Text>
+                                {" "}
+                                <BsSpeedometer className="icon" />
+                                {status}
+                            </Card.Text>
+                        ) : (
+                            ""
+                        )}
+                        {deadline && (
+                            <Card.Text>
+                                {" "}
+                                <BsAlarmFill className="icon" />
+                                {deadlineText}
+                                {deadlineDate}
+                            </Card.Text>
+                        )}
+                        <DropdownButton
+                            variant="secondary"
+                            title="Change Status"
+                        >
+                            {Object.values(Status).map((status) => (
+                                <Dropdown.Item
+                                    key={status}
+                                    onClick={() => {
+                                        if (
+                                            status == Status.OA ||
+                                            status == Status.Interview ||
+                                            status == Status.Offered
+                                        ) {
+                                            setShowModal(true);
+                                        } else {
+                                            updateDeadline(user.uid, id, "");
+                                        }
+                                        updateStatus(user.uid, id, status);
+                                    }}
+                                >
+                                    {status}
+                                </Dropdown.Item>
+                            ))}
+                        </DropdownButton>
+                        <Card.Text>
+                            {" "}
+                            <Button
+                                variant="outline-secondary"
+                                id="trashIcon"
+                                data-testid={`delete-${tab}-${id}`}
+                                onClick={() => removeJob(user.uid, id)}
+                            >
+                                <BsTrash className="icon" />
+                            </Button>
+                        </Card.Text>
+                    </Card.Body>
+                </Collapse>
+            </Card>
+        </div>
+    );
 };
--- src/components/JobsList.yaur.test.jsx
+++ src/components/JobsList.yaur.test.jsx
@@ -1,53 +1,76 @@
-import { describe, it, vi } from 'vitest';
-import { render, screen, fireEvent } from '@testing-library/react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Subheader } from './Subheader';
-import { getUser } from './User';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { describe, it, vi } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
 
-vi.mock('./User');
+import { Subheader } from "./Subheader";
+import { getUser } from "./User";
 
-const mockUser = {    
-        "displayName": "Pika Pikachu",
-        "email": "pikachu@gmail.com", 
-        "jobs": {
-          "0": {
-            "company": "Amazon",
-            "jobTitle": "sde",
-            "appliedOn": "2022-11-14T21:00:00.000Z",
-            "status": "Applied",
-            "deadline": ""
-          },   
-          "1": {
-            "company": "Google",
-            "jobTitle": "swe",
-            "appliedOn": "2022-11-11T21:00:00.000Z",
-            "status": "Interview ",
-            "deadline": "2022-11-20T21:00:00.000Z"
-          },
-          "2": {
-            "company": "Netflix",
-            "jobTitle": "pm",
-            "appliedOn": "2022-11-11T21:00:00.000Z",
-            "status": "Offered ",
-            "deadline": "2022-11-20T21:00:00.000Z"
-          },
-          "3": {
-            "company": "Apple",
-            "jobTitle": "swe",
-            "appliedOn": "2022-11-11T21:00:00.000Z",
-            "status": "Their Loss",
-            "deadline": "2022-11-20T21:00:00.000Z"
-          }
-        }
-}
+vi.mock("./User");
+
+const mockUser = {
+    displayName: "Pika Pikachu",
+    email: "pikachu@gmail.com",
+    jobs: {
+        0: {
+            company: "Amazon",
+            jobTitle: "sde",
+            appliedOn: "2022-11-14T21:00:00.000Z",
+            status: "Applied",
+            deadline: "",
+        },
+        1: {
+            company: "Google",
+            jobTitle: "swe",
+            appliedOn: "2022-11-11T21:00:00.000Z",
+            status: "Interview ",
+            deadline: "2022-11-20T21:00:00.000Z",
+        },
+        2: {
+            company: "Netflix",
+            jobTitle: "pm",
+            appliedOn: "2022-11-11T21:00:00.000Z",
+            status: "Offered ",
+            deadline: "2022-11-20T21:00:00.000Z",
+        },
+        3: {
+            company: "Apple",
+            jobTitle: "swe",
+            appliedOn: "2022-11-11T21:00:00.000Z",
+            status: "Their Loss",
+            deadline: "2022-11-20T21:00:00.000Z",
+        },
+        4: {
+            company: "Bloomberg",
+            jobTitle: "swe",
+            appliedOn: "2022-11-11T21:00:00.000Z",
+            status: "Their Loss",
+            deadline: "2022-11-20T21:00:00.000Z",
+        },
+    },
+};
 
 describe("No jobs with status 'Online Assessment'", () => {
     it("should return 'No jobs with this status'", async () => {
-      getUser.mockReturnValue([mockUser]);
-      await render(<Subheader jobs={mockUser["jobs"]} user={mockUser} />, {wrapper: BrowserRouter})
-      const oaTab = screen.getByTestId("Online Assessment");
-      await fireEvent.click(oaTab);
-      const oaList = screen.getByTestId("Online Assessment List")
-      expect(oaList.textContent).toBe('No jobs with this status')
-      });
-})
+        getUser.mockReturnValue([mockUser]);
+        await render(<Subheader jobs={mockUser["jobs"]} user={mockUser} />, {
+            wrapper: BrowserRouter,
+        });
+        const oaTab = screen.getByTestId("Online Assessment");
+        await fireEvent.click(oaTab);
+        const oaList = screen.getByTestId("Online Assessment List");
+        expect(oaList.textContent).toBe("No jobs with this status");
+    });
+});
+
+describe("'Their Loss' tab only displays jobs with status 'Their Loss'", () => {
+    it("jobs should have status 'Their Loss'", async () => {
+        getUser.mockReturnValue([mockUser]);
+        await render(<Subheader jobs={mockUser["jobs"]} user={mockUser} />, {
+            wrapper: BrowserRouter,
+        });
+        const tlTab = screen.getByTestId("Their Loss");
+        await fireEvent.click(tlTab);
+        const tlList = screen.getByTestId("Their Loss List");
+        expect(Object.values(tlList)[1].children).toHaveLength(2);
+    });
+});
